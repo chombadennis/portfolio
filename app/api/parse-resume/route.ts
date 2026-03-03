fc22fcc4b28c085d2bf3bcad9ad1932863e3faa1
chombadennis <dennis.cmunene@gmail.com>
2026-03-03 10:53:19 +0000

    runtime nodejs added to the parse-resume file

--- app/api/parse-resume/route.ts
+++ app/api/parse-resume/route.ts
@@ -1,7 +1,8 @@
-
 import { NextResponse } from 'next/server';
 import mammoth from 'mammoth';
 const { PDFParse } = require('pdf-parse');
+
+export const runtime = 'nodejs';
 
 export async function POST(req: Request) {
     try {
